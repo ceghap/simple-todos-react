d23897bbbcb6e27fcc84cf5b8d2799cfb41e24cd
Ashraf Latif <hello@ceghap.com>
2021-05-20 17:42:43 +0800

    add select & delete task

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -9,13 +9,28 @@ export const App = () => {
     TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
   );
 
+  const toggleChecked = ({ _id, isChecked }) => {
+    TasksCollection.update(_id, {
+      $set: {
+        isChecked: !isChecked,
+      },
+    });
+  };
+
+  const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
       <TaskForm />
       <ul>
         {tasks.map((task) => (
-          <Task key={task._id} task={task.text} />
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteTask}
+          />
         ))}
       </ul>
     </div>
